f61e1b0e52a2c148edcabebd071c255e3bb718d8
TungVo <tungB2111828@student.ctu.edu.vn>
2025-03-11 18:03:15 +0700

    Cai dat trang loi 404 not found

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Oops, ko the tim thay trang. Tro ve
+            <router-link to="/">Trang chu.</router-link>
+        </p>
+    </div>
+</template>
